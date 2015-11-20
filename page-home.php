9f13928d1e8231b9f38f5603c70cc0d5f5388b73
csnedecor <csnedecor@worthwild.com>
2015-11-20 15:45:26 -0500

    Adds video featurette section using Advanced Custom Fields

--- page-home.php
+++ page-home.php
@@ -35,6 +35,9 @@ $features_section_body    = get_field('features_section_body');
 $project_feature_title    = get_field('project_feature_title');
 $project_feature_body     = get_field('project_feature_body');
 
+$video_featurette_title   = get_field('video_featurette_title');
+$video_featurette_video   = get_field('video_featurette_youtube_video');
+
 get_header(); ?>
   <!-- HERO -->
   <section id="hero" data-type="background" data-speed="5">
@@ -227,8 +230,8 @@ get_header(); ?>
     <div class="container">
       <div class="row">
         <div class="col-sm-8 col-sm-offset-2">
-          <h2>Watch the Course Introduction</h2>
-          <iframe width="100%" height="415" src="//www.youtube.com/embed/q-mJJsnOHew" frameborder="0" allowfullscreen></iframe>
+          <h2><?php echo $video_featurette_title ?></h2>
+          <iframe width="100%" height="415" src="<?php echo $video_featurette_video ?>" frameborder="0" allowfullscreen></iframe>
         </div><!-- end col -->
       </div><!-- row -->
     </div><!-- container -->
