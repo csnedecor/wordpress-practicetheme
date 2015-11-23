c5dda724c0bbfa064b7c09a284eeb2640e7f0577
csnedecor <csnedecor@worthwild.com>
2015-11-23 10:18:20 -0500

    Makes Course Features section into reusible template

--- page-home.php
+++ page-home.php
@@ -3,10 +3,6 @@
   Template Name: Home Page
  */
 
-$features_section_image   = get_field('features_section_image');
-$features_section_title   = get_field('features_section_title');
-$features_section_body    = get_field('features_section_body');
-
 $project_feature_title    = get_field('project_feature_title');
 $project_feature_body     = get_field('project_feature_body');
 
@@ -35,43 +31,7 @@ get_header(); ?>
 
   <?php get_template_part( 'template-parts/content', 'who'); ?>
 
-  <!-- COURSE FEATURES -->
-  <section id="course-features">
-    <div class="container">
-
-      <div class="section-header">
-        <?php if( !empty($features_section_image) ) : ?>
-
-          <img src="<?php echo $features_section_image['url']; ?>" alt="<?php echo $features_section_image['alt']; ?>">
-        <?php endif ?>
-
-        <h2><?php echo $features_section_title; ?></h2>
-
-        <?php if( !empty($features_section_body) ) : ?>
-          <p class="lead"><?php echo $features_section_body; ?></p>
-        <?php endif; ?>
-      </div>
-
-      <div class="row">
-        <!-- Created custom post type using Custom Post Type UI that only supports "Title" , then added custom field radio button to the post type that allows user to choose which icon to use (computer, instructor, watch etc.), then replaces the <i class> with ci ci-computer or ci ci-watch etc to correspond to the correct CSS classes. Syntax for radio buttons in advanced custom field: ci ci-computer : Computer -->
-
-        <!-- Get the course_feature custom post array -->
-        <?php $loop = new WP_Query(array( 'post_type' => 'course_feature', 'order_by' => 'post_id', 'order' => 'ASC')); ?>
-
-        <!-- Loop through each course feature in the "course_features" custom post array -->
-        <?php while($loop->have_posts()) : $loop->the_post(); ?>
-          <div class="col-sm-2">
-            <i class="<?php the_field('course_feature_icon'); ?>"></i>
-            <h3><?php the_title(); ?></h3>
-
-          </div><!-- col -->
-
-        <?php endwhile; ?>
-      </div><!-- row -->
-
-    </div><!-- container -->
-
-  </section><!-- course-features -->
+  <?php get_template_part( 'template-parts/content', 'course-features'); ?>
 
   <!-- PROJECT FEATURES -->
   <section id="project-features">
---
+++ template-parts/content-course-features.php
@@ -0,0 +1,44 @@
+<?php
+  //Advanced Custom Fields
+  //Note: To add Advanced Custom Fields, install the Advanced Custom Fields plugin by Elliot Condon, add field groups, then use the field names it generates in your get_field statement.
+  $features_section_image   = get_field('features_section_image');
+  $features_section_title   = get_field('features_section_title');
+  $features_section_body    = get_field('features_section_body');
+?>
+<!-- COURSE FEATURES -->
+<section id="course-features">
+  <div class="container">
+
+    <div class="section-header">
+      <?php if( !empty($features_section_image) ) : ?>
+
+        <img src="<?php echo $features_section_image['url']; ?>" alt="<?php echo $features_section_image['alt']; ?>">
+      <?php endif ?>
+
+      <h2><?php echo $features_section_title; ?></h2>
+
+      <?php if( !empty($features_section_body) ) : ?>
+        <p class="lead"><?php echo $features_section_body; ?></p>
+      <?php endif; ?>
+    </div>
+
+    <div class="row">
+      <!-- Created custom post type using Custom Post Type UI that only supports "Title" , then added custom field radio button to the post type that allows user to choose which icon to use (computer, instructor, watch etc.), then replaces the <i class> with ci ci-computer or ci ci-watch etc to correspond to the correct CSS classes. Syntax for radio buttons in advanced custom field: ci ci-computer : Computer -->
+
+      <!-- Get the course_feature custom post array -->
+      <?php $loop = new WP_Query(array( 'post_type' => 'course_feature', 'order_by' => 'post_id', 'order' => 'ASC')); ?>
+
+      <!-- Loop through each course feature in the "course_features" custom post array -->
+      <?php while($loop->have_posts()) : $loop->the_post(); ?>
+        <div class="col-sm-2">
+          <i class="<?php the_field('course_feature_icon'); ?>"></i>
+          <h3><?php the_title(); ?></h3>
+
+        </div><!-- col -->
+
+      <?php endwhile; ?>
+    </div><!-- row -->
+
+  </div><!-- container -->
+
+</section><!-- course-features -->
