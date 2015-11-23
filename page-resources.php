fc4e86bbdf421b3d9934cebd3da5c7cab671523b
csnedecor <csnedecor@worthwild.com>
2015-11-23 12:01:31 -0500

    Makes it so user can upload header image as the featured image for the page with a default if they don't

--- page-resources.php
+++ page-resources.php
@@ -2,13 +2,21 @@
 // Template Name: Resources
 
 get_header();
+
+$thumbnail_url    = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );
 ?>
 
 <!-- FEATURE IMAGE
 ================================================== -->
-<section class="feature-image feature-image-default" data-type="background" data-speed="2">
-  <h1>Resources</h1>
-</section>
+<?php if( has_post_thumbnail() ) { ?> <!-- check for feature image -->
+  <section class="feature-image" style="background: url('<?php echo $thumbnail_url ?>') no-repeat; background-size: cover;" data-type="background" data-speed="2">
+    <h1><?php the_title(); ?></h1>
+  </section>
+<?php } else { ?>
+  <section class="feature-image feature-image-default" data-type="background" data-speed="2">
+    <h1><?php the_title(); ?></h1>
+  </section>
+<?php } ?>
 
 
   <!-- MAIN CONTENT
