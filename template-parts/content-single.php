8a57f700c048eaf1e01837f7adaf40c436a654ef
csnedecor <csnedecor@worthwild.com>
2015-11-24 08:50:35 -0500

    copy content from content.php to content-single.php and change it to show the whole blog post (the_content instead of the_excerpt)

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -11,25 +11,34 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
 
-		<div class="entry-meta">
-			<?php bootstrap2wordpress_posted_on(); ?>
-		</div><!-- .entry-meta -->
+		<?php if ( 'post' === get_post_type() ) : ?>
+			<div class="post-details">
+        <i class="fa fa-user"></i> <?php the_author(); ?>
+        <i class="fa fa-clock-o"></i> <time><?php the_date(); ?></time>
+        <i class="fa fa-folder"></i> <?php the_category(", "); ?> <!--list the categories separated by a comma. -->
+        <i class="fa fa-tags"></i> <?php the_tags(); ?> <!-- list the tags -->
+
+        <div class="post-comments-badge">
+
+        	<!-- comments_number syntax: comments_number($zero, $one, $more) where $zero is what you want to display if there are 0 comments. Default values are $zero: "No Comments", $one: "One Comment", $more: "X Comments" -->
+          <a href="<?php comments_link(); ?>"><i class="fa fa-comments"></i> <?php comments_number( 0, 1, "%"); ?><!-- percent sign pulls in the number of comments --></a>
+        </div><!-- post-comments-badge -->
+
+        <!-- Add link to allow author to edit from page. Syntax:  edit_post_link($text, $before_text, $after_text)-->
+        <div><?php edit_post_link("Edit", "<div><i class='fa fa-pencil'></i>", "</div>"); ?></div>
+      </div><!-- post-details -->
+		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bootstrap2wordpress' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php bootstrap2wordpress_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+	<?php if ( has_post_thumbnail() ) { //check for featured image?>
+	  <div class="post-image">
+	    <?php the_post_thumbnail(); ?>
+	  </div><!-- post-image -->
+	<?php } ?>
+  <div class="post-body">
+    <?php the_content(); ?>
+  </div><!-- post-body -->
 </article><!-- #post-## -->
 
